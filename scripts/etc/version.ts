d314c2a773fe7562ebc3b173871ba1a90400b233
hg.park <hangil6061@naver.com>
2019-04-09 18:04:20 +0900

    버전 확인 스크립트 추가

---
+++ scripts/etc/version.ts
@@ -0,0 +1,35 @@
+readTextFile( 'version.json', ( text )=>{
+    printVersion(JSON.parse(text));
+} );
+
+function readTextFile(file, call) {
+    const req = new XMLHttpRequest();
+    req.open("GET", file, true);
+    req.onreadystatechange = function() {
+        if( req.readyState === 4 && req.status === 200 ) {
+            call && call( req.responseText );
+        }
+    };
+    req.send(null);
+}
+
+function printVersion( version ) {
+    const color1 = '#f4f4f4';
+    const bgColor1 = '#bb2525';
+    const color2 = '#fff560';
+    const bgColor2 = '#030307';
+
+
+    if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+        const args = [
+            '%c '+ version.title+ ' %c v' + version.version + ' ',
+            'color: '+color1+'; background: '+bgColor1+'; padding:2px 0;',
+            'color: '+color2+'; background: '+bgColor2+'; padding:2px 0;',
+        ];
+
+        window.console.log.apply(console, args);
+    } else if (window.console)
+    {
+        window.console.log( version.title + ' - v' + version.version);
+    }
+}
